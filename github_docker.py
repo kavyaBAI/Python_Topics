9ec52b73c7d16653f19cab80791928a70bbc0a82
kavya <kavyaadithi8787@gmail.com>
2025-01-30 11:48:47 +0530

    github file

---
+++ github_docker.py
@@ -0,0 +1,7 @@
+#git is a local respository ,github is centralized repository
+#.git (will create empty local repo)
+# git add . or (specific file_name) --> will add to staging area
+#git status (will be green) -->it is moved to stating area 
+#git commit -m to "what changes u have or which file u r moving" 
+# git status (it should come all clear)
+#when u commit it will moved into ur local repo, now i have to move 
